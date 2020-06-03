e287e33bc1d225518358d1444cdd75ebce2a6c1b
shoichi.sugiyama <syouiti0722@gmail.com>
2020-06-03 12:10:38 +0000

    timeline

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,5 +1,8 @@
 class Micropost < ApplicationRecord
   belongs_to :user
+  has_many :favorites
+  has_many :reverses_of_favorite, class_name: 'Favorite', foreign_key: 'user_id'
   
   validates :content, presence: true, length: { maximum: 255 }
+  
 end
